dcd387359c80397aab480c28292f61b4189dcbaf
thanhlh05 <lht0456@gmail.com>
2026-09-24 19:00:56 +0700

    feat: add initial game state

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import './style.css';
 import { gameState } from './game/gameState.js';
 
-console.log('Xe Bánh Mì Nhỏ - Game Loaded!', gameState);
+console.log('Xe bánh mì - Game Loaded!', gameState);
